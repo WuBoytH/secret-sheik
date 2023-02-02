fcc886e63230e4d23998f2aaf8ac2fa56548d127
WuBoytH <alexwuief@gmail.com>
2023-02-01 23:50:03 -0500

    Disabled Throw Edits

--- src/sheik/acmd/throws.rs
+++ src/sheik/acmd/throws.rs
@@ -84,72 +84,72 @@ unsafe fn game_catchturn(fighter: &mut L2CAgentBase) {
     macros::FT_MOTION_RATE(fighter, 1.03);
 }
 
-#[acmd_script( agent = "sheik", script = "game_throwf", category = ACMD_GAME, low_priority )]
-unsafe fn game_throwf(fighter: &mut L2CAgentBase) {
-    if macros::is_excute(fighter) {
-        // Throw damage increased from 2 to 8
-        // KBG decreased from 72 to 30
-        // Hitstun *should* be increased anyway from the damage increase,
-        // but we can't increase the hitstun of throws directly
-        macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 8.0, 30, 79, 0, 80, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
-        macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 60, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
-    }
-    frame(fighter.lua_state_agent, 20.0);
-    if macros::is_excute(fighter) {
-        // Hitbox damage increased from 5 to 8
-        macros::ATTACK(fighter, 0, 0, Hash40::new("shoulderl"), 8.0, 361, 0, 80, 10, 5.8, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
-        AttackModule::set_catch_only_all(fighter.module_accessor, true, false);
-        macros::CHECK_FINISH_CAMERA(fighter, 10, 2);
-    }
-    frame(fighter.lua_state_agent, 22.0);
-    if macros::is_excute(fighter) {
-        let target = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
-        let target_group = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
-        let target_no = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
-        macros::ATK_HIT_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
-        AttackModule::clear_all(fighter.module_accessor);
-    }
-}
+// #[acmd_script( agent = "sheik", script = "game_throwf", category = ACMD_GAME, low_priority )]
+// unsafe fn game_throwf(fighter: &mut L2CAgentBase) {
+//     if macros::is_excute(fighter) {
+//         // Throw damage increased from 2 to 8
+//         // KBG decreased from 72 to 30
+//         // Hitstun *should* be increased anyway from the damage increase,
+//         // but we can't increase the hitstun of throws directly
+//         macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 8.0, 30, 79, 0, 80, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+//         macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 60, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+//     }
+//     frame(fighter.lua_state_agent, 20.0);
+//     if macros::is_excute(fighter) {
+//         // Hitbox damage increased from 5 to 8
+//         macros::ATTACK(fighter, 0, 0, Hash40::new("shoulderl"), 8.0, 361, 0, 80, 10, 5.8, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
+//         AttackModule::set_catch_only_all(fighter.module_accessor, true, false);
+//         macros::CHECK_FINISH_CAMERA(fighter, 10, 2);
+//     }
+//     frame(fighter.lua_state_agent, 22.0);
+//     if macros::is_excute(fighter) {
+//         let target = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
+//         let target_group = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
+//         let target_no = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
+//         macros::ATK_HIT_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
+//         AttackModule::clear_all(fighter.module_accessor);
+//     }
+// }
 
-#[acmd_script( agent = "sheik", script = "game_throwlw", category = ACMD_GAME, low_priority )]
-unsafe fn game_throwlw(fighter: &mut L2CAgentBase) {
-    frame(fighter.lua_state_agent, 1.0);
-    if macros::is_excute(fighter) {
-        macros::FT_LEAVE_NEAR_OTTOTTO(fighter, -2.5, 2.5);
-        // Throw damage increased from 3 to 8
-        // KBG decreased from 77 to 24
-        // Hitstun *should* be increased anyway from the damage increase,
-        // but we can't increase the hitstun of throws directly
-        macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 8.0, 24, 60, 0, 95, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
-        macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
-    }
-    frame(fighter.lua_state_agent, 26.0);
-    if macros::is_excute(fighter) {
-        // Throw damage increased from 3 to 8
-        macros::ATTACK(fighter, 0, 0, Hash40::new("kneer"), 8.0, 361, 0, 100, 10, 5.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
-        macros::ATTACK(fighter, 1, 0, Hash40::new("footr"), 8.0, 361, 0, 100, 10, 6.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
-        AttackModule::set_catch_only_all(fighter.module_accessor, true, false);
-    }
-    wait(fighter.lua_state_agent, 1.0);
-    if macros::is_excute(fighter) {
-        macros::CHECK_FINISH_CAMERA(fighter, -1, 0);
-    }
-    wait(fighter.lua_state_agent, 4.0);
-    if macros::is_excute(fighter) {
-        AttackModule::clear_all(fighter.module_accessor);
-        let target = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
-        let target_group = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
-        let target_no = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
-        macros::ATK_HIT_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
-    }
-}
+// #[acmd_script( agent = "sheik", script = "game_throwlw", category = ACMD_GAME, low_priority )]
+// unsafe fn game_throwlw(fighter: &mut L2CAgentBase) {
+//     frame(fighter.lua_state_agent, 1.0);
+//     if macros::is_excute(fighter) {
+//         macros::FT_LEAVE_NEAR_OTTOTTO(fighter, -2.5, 2.5);
+//         // Throw damage increased from 3 to 8
+//         // KBG decreased from 77 to 24
+//         // Hitstun *should* be increased anyway from the damage increase,
+//         // but we can't increase the hitstun of throws directly
+//         macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 8.0, 24, 60, 0, 95, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+//         macros::ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+//     }
+//     frame(fighter.lua_state_agent, 26.0);
+//     if macros::is_excute(fighter) {
+//         // Throw damage increased from 3 to 8
+//         macros::ATTACK(fighter, 0, 0, Hash40::new("kneer"), 8.0, 361, 0, 100, 10, 5.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
+//         macros::ATTACK(fighter, 1, 0, Hash40::new("footr"), 8.0, 361, 0, 100, 10, 6.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
+//         AttackModule::set_catch_only_all(fighter.module_accessor, true, false);
+//     }
+//     wait(fighter.lua_state_agent, 1.0);
+//     if macros::is_excute(fighter) {
+//         macros::CHECK_FINISH_CAMERA(fighter, -1, 0);
+//     }
+//     wait(fighter.lua_state_agent, 4.0);
+//     if macros::is_excute(fighter) {
+//         AttackModule::clear_all(fighter.module_accessor);
+//         let target = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
+//         let target_group = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
+//         let target_no = WorkModule::get_int64(fighter.module_accessor, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
+//         macros::ATK_HIT_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
+//     }
+// }
 
 pub fn install() {
     install_acmd_scripts!(
         game_catch,
         game_catchdash,
         game_catchturn,
-        game_throwf,
-        game_throwlw
+        // game_throwf,
+        // game_throwlw
     );
 }
